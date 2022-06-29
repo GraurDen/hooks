3f5d55b64f71ac0def06e83aab44824982f6bf65
GraurDen <graurden@gmail.com>
2022-06-29 22:02:19 +0300

    add function adNumbers

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+//https://youtu.be/8Yy6MDsF-Tg
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [numbers, setNumbers] = useState([(1, 2, 3, 4, 5)]);
+
+    function addNumber() {
+        const lastNumber = numbers[numbers.length - 1];
+        setNumbers([...numbers, lastNumber + 1]);
+    }
+
+    console.log(numbers);
+
+    return (
+        <div className="App">
+            <button onClick={addNumber}>add number</button>
+        </div>
+    );
 }
 
 export default App;
