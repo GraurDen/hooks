2dc424bbd91c2c5019e70442f7d16acb051c12e8
GraurDen <graurden@gmail.com>
2022-06-30 23:05:40 +0300

    add addNumbers

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-//https://youtu.be/8Yy6MDsF-Tg
+//https://youtu.be/8Yy6MDsF-Tg?t=502
 
 function App() {
-    const [numbers, setNumbers] = useState([(1, 2, 3, 4, 5)]);
+    const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+    let ulElem;
 
     function addNumber() {
         const lastNumber = numbers[numbers.length - 1];
@@ -14,9 +15,31 @@ function App() {
 
     console.log(numbers);
 
+    const handleScroll = () => {
+        console.log("that was scrolling");
+    };
+
+    useEffect(() => {
+        ulElem = document.getElementsByClassName("block")[0];
+        ulElem.addEventListener("scroll", handleScroll);
+    }, []);
+
+    const removescroll = () => {
+        ulElem.removeEventListener("scroll", handleScroll);
+    };
+
     return (
         <div className="App">
+            <div className="block">
+                <ul>
+                    {numbers.map((num) => {
+                        return <li key={num}>{num}</li>;
+                    })}
+                </ul>
+            </div>
+
             <button onClick={addNumber}>add number</button>
+            <button onClick={removescroll}>remove scroll</button>
         </div>
     );
 }
