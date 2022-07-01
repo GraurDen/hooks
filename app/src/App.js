e7452d1f3b2b252c43615d0994096cd370e2ca28
Alexandr Kingstam <kingstam@gmail.com>
2022-07-01 18:19:47 +0300

    add use ref

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 //https://youtu.be/8Yy6MDsF-Tg?t=502
 
 function App() {
     const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
-    let ulElem;
+    const ulElem = useRef();
 
     function addNumber() {
         const lastNumber = numbers[numbers.length - 1];
         setNumbers([...numbers, lastNumber + 1]);
     }
+
+    useEffect(() => {
+        ulElem.current.addEventListener('scroll', handleScroll)
+    }, []);
 
     console.log(numbers);
 
@@ -19,18 +23,16 @@ function App() {
         console.log("that was scrolling");
     };
 
-    useEffect(() => {
-        ulElem = document.getElementsByClassName("block")[0];
-        ulElem.addEventListener("scroll", handleScroll);
-    }, []);
-
     const removescroll = () => {
-        ulElem.removeEventListener("scroll", handleScroll);
+        ulElem.current.removeEventListener("scroll", handleScroll);
+        console.log('remove scroll')
     };
+
+
 
     return (
         <div className="App">
-            <div className="block">
+            <div className="block" ref={ulElem}>
                 <ul>
                     {numbers.map((num) => {
                         return <li key={num}>{num}</li>;
